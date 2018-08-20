2690a009615d360162df06efa9ca5a7bd5a62719
Dimitri Couwenberg <dimitricouwenberg@192-168-1-101.tpgi.com.au>
2018-08-20 11:32:58 +1000

    feat: added people endpoint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Universal Demo using Angular and Angular CLI</h1>
-  <a routerLink="/">Home</a>
+  <h1>Universal Demo using Angular blablablaAngular CLI</h1>
+  <a routerLink="/">Homefdsfdsfds</a>
   <a routerLink="/lazy">Lazy</a>
   <a routerLink="/lazy/nested">Lazy_Nested</a>
   <router-outlet></router-outlet>
